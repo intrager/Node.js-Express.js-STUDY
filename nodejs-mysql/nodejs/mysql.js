7cb900e5a977038caf3d20011587f2b84ae53486
Bruce Han <intrager@naver.com>
2022-01-07 02:14:03 +0900

    MySQL 연동 및 CRUD 완료

--- nodejs-mysql/nodejs/mysql.js
+++ nodejs-mysql/nodejs/mysql.js
@@ -1,9 +1,10 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',   // database 서버가 어떤 컴퓨터에 있는가
-  user     : 'root',
-  password : 'shem12',
-  database : 'opentutorials'    // 본인이 쓸 database
+  user     : 'nodejs',
+  password : 'shem',
+  database : 'opentutorials',    // 본인이 쓸 database
+  port: 3307  // 3306인줄 알았지만 3307이었다.
 });
  
 connection.connect();
@@ -16,3 +17,4 @@ connection.query('SELECT * FROM topic', function (error, results, fields) {
 });
  
 connection.end();
+// 2022-01-06. 01-03부터 시작한 mysql 설치 이제 됐다! 야!!! Tlqkf

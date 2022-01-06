bbbe505a9568bda6b9402db1fbbf5e39636e67f4
Bruce Han <intrager@naver.com>
2022-01-06 23:40:18 +0900

    연습중 3

--- nodejs-mysql/nodejs/mysql.js
+++ nodejs-mysql/nodejs/mysql.js
@@ -2,13 +2,13 @@ var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',   // database 서버가 어떤 컴퓨터에 있는가
   user     : 'root',
-  password : 'shemdustmq12',
+  password : 'shem12',
   database : 'opentutorials'    // 본인이 쓸 database
 });
  
 connection.connect();
  
-connection.query('SELECT version()', function (error, results, fields) {
+connection.query('SELECT * FROM topic', function (error, results, fields) {
   if (error) {
     console.log(error);
   } 
